b22486773b4af3d6b9be255f0fefa4a5ee094570
khalil hadjahmed <khalil.hytex@gmail.com>
2024-07-20 13:02:28 +0100

    initial commit

---
+++ src/main/java/com/springboot/algerian_football_app/AlgerianFootballAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.algerian_football_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgerianFootballAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgerianFootballAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/algerian_football_app/AlgerianFootballAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.algerian_football_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgerianFootballAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
